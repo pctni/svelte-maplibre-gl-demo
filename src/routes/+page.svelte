a00c339d07194faf441140d66df69ebabcfb768b
roblinlovelace <rob00x@gmail.com>
2025-07-29 14:17:16 +0100

    Add scale bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { MapLibre, FullScreenControl, GeolocateControl } from 'svelte-maplibre-gl';
+  import { MapLibre, FullScreenControl, GeolocateControl, ScaleControl } from 'svelte-maplibre-gl';
 </script>
 
 <MapLibre class="h-[calc(100vh-100px)]" style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json">
   <FullScreenControl position="top-left" />
   <GeolocateControl position="top-left" />
+  <ScaleControl position="bottom-left" unit="metric" maxWidth=200/>
 </MapLibre>
