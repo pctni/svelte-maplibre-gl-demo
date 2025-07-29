84b792efa00ae55751eaa45fa7b4438a0a8b7e1d
roblinlovelace <rob00x@gmail.com>
2025-07-29 14:00:57 +0100

    Tidy-up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 
 	<footer>
 		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+			<!-- Removed SvelteKit documentation link -->
 		</p>
 	</footer>
 </div>
